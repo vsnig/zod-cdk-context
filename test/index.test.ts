302fe41a9e2bbc29fd1d41231bda6d36836cb2cd
Viacheslav Snizhko <snegostoop@gmail.com>
2024-10-14 16:27:43 +0300

    better test

--- test/index.test.ts
+++ test/index.test.ts
@@ -5,22 +5,27 @@ import { z } from 'zod'
 import { getValidatedContext } from '../src/index.js'
 
 describe('validated context', () => {
-  test('get and validate context', () => {
+  test('get string context value', () => {
     const app = new cdk.App({ context: { foo: 'bar' } })
     const { foo } = getValidatedContext(app.node, { foo: z.string() })
     expect(foo).toEqual('bar')
   })
 
-  test('get and validate context', () => {
+  test('get number context value', () => {
     const app = new cdk.App({ context: { foo: '99' } })
-    const { foo } = getValidatedContext(app.node, { foo: z.coerce.number() })
+    const values = getValidatedContext(app.node, { foo: z.coerce.number() })
 
-    expect(foo).toEqual(99)
+    expect(values).toEqual({ foo: 99 })
   })
 
-  test('throws on wrong key', () => {
-    const app = new cdk.App({ context: { foo: 'bar' } })
-    // @ts-expect-error foo indeed is not in object
-    expect(() => getValidatedContext(app.node, { buzz: z.string() }).foo).toThrow()
+  test("pass when optional value doesn't exist in context", () => {
+    const app = new cdk.App({ context: {} })
+    const values = getValidatedContext(app.node, { buzz: z.string().optional() })
+    expect(values).toEqual({})
+  })
+
+  test("throw when required value doesn't exist in context", () => {
+    const app = new cdk.App({ context: {} })
+    expect(() => getValidatedContext(app.node, { buzz: z.string() })).toThrow()
   })
 })
